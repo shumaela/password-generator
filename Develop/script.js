8dec18dc99b1b7239d62044264a9ad94cb82b846
shumaela101~ <shumu.laeeq@gmail.com>
2023-12-29 20:04:02 -0500

    added functions to generate/write password according to 8-128 length

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// function to generate a random password
+function generatePassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
 
+  // checks to see if the password length is between the requested range of 8-128 characters long
+  if (length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
 }
+
+// write password to the password input 
+function writePassword() {
+  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):")
+}
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
