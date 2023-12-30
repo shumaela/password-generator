29f7ba3365b0260863a167b091552f60d83dd047
shumaela101~ <shumu.laeeq@gmail.com>
2023-12-29 21:36:37 -0500

    comments edits

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,11 @@ var generateBtn = document.querySelector("#generate");
 
 // function to generate a random password
 function generatePassword(length) {
-//  defines the character set
+  //  defines the character set
   const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
 
   // checks to see if the password length is between the requested range of 8-128 characters long
-  if (length < 8 || length > 128 || isNaN (length)) {
+  if (length < 8 || length > 128 || isNaN(length)) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return "";
   }
@@ -26,12 +26,11 @@ function generatePassword(length) {
 
 // write password to the password input 
 function writePassword() {
-  // prompts the user for the required password length
   var password = prompt("Enter the desired password:");
 
 
   // check if the length is within the specified range
-  if (password.length >=8 && password.length <=128) {
+  if (password.length >= 8 && password.length <= 128) {
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   } else {
