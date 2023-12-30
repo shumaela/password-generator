0feeaaabc763ff866e324451286b3eefe0abfdfc
shumaela101~ <shumu.laeeq@gmail.com>
2023-12-29 21:33:03 -0500

    multiple edits, fixed length response issue, added comments, fixed random characters issue, displays to page accurately

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,18 @@ var generateBtn = document.querySelector("#generate");
 
 // function to generate a random password
 function generatePassword(length) {
+//  defines the character set
   const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
 
   // checks to see if the password length is between the requested range of 8-128 characters long
-  if (length < 8 || length > 128) {
+  if (length < 8 || length > 128 || isNaN (length)) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return "";
   }
 
+  // generates the password with random letters and numbers
   let password = "";
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset.charAt(randomIndex);
@@ -23,17 +26,19 @@ function generatePassword(length) {
 
 // write password to the password input 
 function writePassword() {
-  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):")
+  // prompts the user for the required password length
+  var password = prompt("Enter the desired password:");
+
+
+  // check if the length is within the specified range
+  if (password.length >=8 && password.length <=128) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  } else {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+  }
 }
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
